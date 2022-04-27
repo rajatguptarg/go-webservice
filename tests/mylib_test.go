44c02b34d78d7328399aa0a951be087e2aa875db
Rajat Gupta <rajat.gupta712@gmail.com>
2022-04-27 21:44:24 +0200

    Added test suites

---
+++ tests/mylib_test.go
@@ -0,0 +1,19 @@
+package tests
+
+import (
+	"testing"
+)
+
+func Test_BasicChecks(t *testing.T) {
+	t.Parallel()
+	t.Run("Go can add", func(t *testing.T) {
+		if 1+1 != 2 {
+			t.Fail()
+		}
+	})
+	t.Run("Go can concatenate strings", func(t *testing.T) {
+		if "Hello, "+"Go" != "Hello, Go" {
+			t.Fail()
+		}
+	})
+}
